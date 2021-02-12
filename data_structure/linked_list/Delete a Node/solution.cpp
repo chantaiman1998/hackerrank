b6518847e771d2694d80f6eea23226c0e945242d
tai man chan <46643763+chantaiman1998@users.noreply.github.com>
2021-02-13 01:35:02 +0800

    Update solution.cpp

--- data_structure/linked_list/Delete a Node/solution.cpp
+++ data_structure/linked_list/Delete a Node/solution.cpp
@@ -43,6 +43,66 @@ void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& 
 }
 
 void free_singly_linked_list(SinglyLinkedListNode* node) {
-    while
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+        free(temp);
+    }
+}
+
+// complete the deleteNode function below.
+SinglyLinkedListNode* deleteNode(SinglyLinkedListNode* head, int position) {
+    // if we need to delete the head
+    if (positition == 0) {
+        SinglyLinkedListNode* next = head->next;
+        delete head;
+        return next;
+    }
+    
+    // for any other node to delete
+    SinglyLinkedListNode* temp=head;
+    SinglyLinkedListNode* prev;
+    int pos = 0;
+    while(temp != nullptr) {
+        if (pos == position) break;
+        prev = temp;
+        temp = temp->next;
+        pos++;
+    }
+    prev->next = temp->next;
+    delete temp;
+    return head;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+    SinglyLinkedList* llist = new SinglyLinkedList();
+    int llist_count;
+    cin >> llist_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    for (int i = 0; i < llist_count; i++) {
+        int llist_item;
+        cin >> llist_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        llist->insert_node(llist_item);
+    }
+    int position;
+    cin >> position;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    SinglyLinkedListNOde* llist1 = deleteNode(llist->head, position);
+    print_singly_linked_list(llist1, " ", fout);
+    fout << "\n";
+    free_singly_linked_list(llist1);
+    fout.close();
+    return 0;
+}
+
+
+  
+  
+      
+
+
 
 
