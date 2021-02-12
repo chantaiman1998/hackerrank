2582b9202652539b803797afefc63c80704d62f0
tai man chan <46643763+chantaiman1998@users.noreply.github.com>
2021-02-12 23:21:11 +0800

    Create solution.cpp

---
+++ data_structure/linked_list/Delete a Node/solution.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+  public:
+      int data;
+      SinglyLinkedListNode *next;
+      SinglyLinkedListNode(int node_data) {
+          this->data = nullptr;
+          this->data = nullptr;
+      }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+        void insert_node(int node_data)
+        {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+        node = node->next;
+        if (node) {
+            fout << sep;
+        }
+   }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while
+
+
