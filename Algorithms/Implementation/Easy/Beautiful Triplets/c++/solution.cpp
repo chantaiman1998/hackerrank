2b7a69027ff1cdb5ba668bc10feec158cd3e78db
tai man chan <46643763+chantaiman1998@users.noreply.github.com>
2021-02-28 22:58:49 +0800

    Create solution.cpp

---
+++ Algorithms/Implementation/Easy/Beautiful Triplets/c++/solution.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the beautifulTriplets function below.
+/*
+int beautifulTriplets(int d, vector<int> arr) {
+
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string nd_temp;
+    getline(cin, nd_temp);
+
+    vector<string> nd = split_string(nd_temp);
+
+    int n = stoi(nd[0]);
+
+    int d = stoi(nd[1]);
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int result = beautifulTriplets(d, arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
+*/
+int main(){
+    int sum = 0;
+    int n, d;
+    
+    scanf("%d %d", &n, &d);
+    int seq[n];
+    for(int i = 0; i < n; i++)
+        scanf("%d", &seq[i]);
+        
+    for(int i = 0; i < n-2; ++i){
+        for(int j = i+1; j < n-1; ++j){
+            if(seq[j]-seq[i] == d){
+                for(int k = i+2; k < n; ++k){
+                    if(seq[k]-seq[j] == d){
+                        ++sum;
+                        break;   
+                    }         
+                }
+                break;
+            }
+        }
+    }
+    printf("%d", sum);
+    return 0;
+}
