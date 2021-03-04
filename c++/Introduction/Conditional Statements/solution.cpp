6ea5455ff8dce038f1663eb64995547152401ca2
tai man chan <46643763+chantaiman1998@users.noreply.github.com>
2021-03-04 22:48:03 +0800

    Create solution.cpp

---
+++ c++/Introduction/Conditional Statements/solution.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // array 
+    string a[] = {
+        "Greater than 9","one","two","three","four","five","six","seven","eight","nine"
+    };
+
+    int n;
+    cin >> n;
+    cout<<((n>9)?a[0]:a[n]);
+    // Write Your Code Here
+
+    return 0;
+}
+
