df4b2992c353d7ab441fa4825391103b0353913c
tai man chan <46643763+chantaiman1998@users.noreply.github.com>
2021-05-09 00:17:33 +0800

    Create Solution_tbc.cpp

---
+++ Algorithms/Strings/HackerRank_in_a_String!/Solution_tbc.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'hackerrankInString' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+// word to find
+const string WORD = "hackerrank";
+
+// complete the hackerrankInString function below.
+string hackerrankInString(string s) {
+    int pos = 0;
+
+
