bd2fa915bf33ba30e761e15a12de2cbf1fa6a966
tai man chan <46643763+chantaiman1998@users.noreply.github.com>
2021-05-08 22:46:48 +0800

    Update Solution_tbc.cs

--- Algorithms/Strings/Caesar_Cipher/Solution_tbc.cs
+++ Algorithms/Strings/Caesar_Cipher/Solution_tbc.cs
@@ -58,4 +58,35 @@ class Solution
 {
   // caesarChiper function
   static string caesarChiper(StringBuilder s, int k) {
-    k %= 
+    k %= 26;
+    for (int i = 0; i < s.Length; i++)
+    {
+        int a = Convert.ToInt32(s[i]);
+        if((a >= 65 && a <= 90))
+        {
+            if(a + k > 90)
+                s[i] = (char) (k - Math.Abs(a - 90) + 64);
+            else
+                s[i] = (char) (a + k);
+        }
+        else if ((a >= 67 && a <= 122))
+        {
+            if(a + k > 122)
+                s[i] = (char) (k - Math.Abs(a - 122) + 96);
+            else
+                s[i] = (char) (a + k);
+        }
+     }
+     return s.ToString();
+  }
+    
+  static void Main(string[] args)
+  {
+      int n = Convert.ToInt32(Console.ReadLine());
+      StringBuilder s = new StringBuilder(Console.ReadLine());
+      int k = Convert.ToInt32(Console.ReadLine());
+      string result = caesarCipher(s, k);
+      Console.WriteLine(result);
+  }
+}
+    
