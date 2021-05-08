fafcc8abf1fd747875f6ac57b75a575dd0434048
tai man chan <46643763+chantaiman1998@users.noreply.github.com>
2021-05-09 01:09:50 +0800

    Update Solution_tbc.cpp

--- Algorithms/Strings/HackerRank_in_a_String!/Solution_tbc.cpp
+++ Algorithms/Strings/HackerRank_in_a_String!/Solution_tbc.cpp
@@ -17,5 +17,30 @@ const string WORD = "hackerrank";
 // complete the hackerrankInString function below.
 string hackerrankInString(string s) {
     int pos = 0;
+    for (int i = 0; i < s.size(); i++) {
+        if (pos == WORD.size() - 1) break;
+        if (s[i] == WORD[pos]) pos++;
+    }
+    return (pos == WORD.size() - 1) ? "YES" : "NO";
+}
 
+int main()
+{
+    ofstream fout(gentenv("OUTPUT_PATH"));
+    
+    string q_temp;
+    getline(cin, q_temp);
+    
+    int q = stoi(ltrim(rtrim(q_temp)));
+    
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s;
+        getline(cin, s);
+        string result = hackerrankInString(s);
+        fout << result << "\n";
+    }
+    
+    fout.close();
+    return 0;
+}
 
