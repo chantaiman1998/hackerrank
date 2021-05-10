aa952ec355b64bcf93cfa6736731cc68a1205ded
tai man chan <46643763+chantaiman1998@users.noreply.github.com>
2021-05-11 01:05:45 +0800

    Create Solution_tbc.cs

---
+++ Algorithms/Strings/Weighted_Uniform_Strings/Solution_tbc.cs
@@ -0,0 +1,68 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+    /*
+class Result
+{
+
+
+     * Complete the 'weightedUniformStrings' function below.
+     *
+     * The function is expected to return a STRING_ARRAY.
+     * The function accepts following parameters:
+     *  1. STRING s
+     *  2. INTEGER_ARRAY queries
+
+
+    public static List<string> weightedUniformStrings(string s, List<int> queries)
+    {
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        int queriesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> queries = new List<int>();
+
+        for (int i = 0; i < queriesCount; i++)
+        {
+            int queriesItem = Convert.ToInt32(Console.ReadLine().Trim());
+            queries.Add(queriesItem);
+        }
+
+        List<string> result = Result.weightedUniformStrings(s, queries);
+
+        textWriter.WriteLine(String.Join("\n", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
+  
+  */
+     
+   class Solution {
+      static void Main(String[] args){
+          string s = Console.ReadLine();
+          
+     
+     
