ff642b00cee150b28a9e4861fd4850739f4f2034
tai man chan <46643763+chantaiman1998@users.noreply.github.com>
2021-05-12 00:30:13 +0800

    Update Solution_tbc.cs

--- Algorithms/Strings/Weighted_Uniform_Strings/Solution_tbc.cs
+++ Algorithms/Strings/Weighted_Uniform_Strings/Solution_tbc.cs
@@ -63,6 +63,28 @@ class Solution
    class Solution {
       static void Main(String[] args){
           string s = Console.ReadLine();
+          int n = Convert.ToInt32(Console.ReadLine());
+          var hash = new HashSet<int>();
+          int weight = -1;
+          int count = 0;
+          foreach(var ch in s)
+          {
+              var w = ch - 'a' + 1;
+              if(weight != w)
+              {
+                  weight = w;
+                  count = 0;
+              }
+              count++;
+              hash.Add(weight + count);
+          }
+          for (int a0 = 0; a0 < n; a0++){
+              int x = Convert.ToInt32(Console.ReadLine());
+              Console.WriteLine(hash.Contains(x)?"Yes": "No");
+          }
+      }
+  }
           
      
      
+          {
