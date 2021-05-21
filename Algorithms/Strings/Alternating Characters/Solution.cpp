845d1baa508579498c5da139f6c0fe5afe87a1ff
tai man chan <46643763+chantaiman1998@users.noreply.github.com>
2021-05-21 18:55:16 +0800

    Update Solution.cpp

--- Algorithms/Strings/Alternating Characters/Solution.cpp
+++ Algorithms/Strings/Alternating Characters/Solution.cpp
@@ -72,4 +72,18 @@ using namespace std;
 int main(){
   int t;
   cin >> t;
-  while(t--
+  while(t--){
+      string str;
+      cin >> str;
+      int ans = 0;
+      for(int i = 0; i < str.length() - 1; i++)
+      {
+          if(str[i]==str[i+1]){
+              // if two consecutive characters are same, del one item.
+            ans++;
+          }
+      }
+      cout << ans << endl;
+  }
+  return 0;
+}
