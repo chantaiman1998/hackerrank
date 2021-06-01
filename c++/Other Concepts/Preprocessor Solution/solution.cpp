27dcd531e083cf9c7a4589e1cd69605b508d073a
tai man chan <46643763+chantaiman1998@users.noreply.github.com>
2021-06-01 22:21:08 +0800

    Update solution.cpp

--- c++/Other Concepts/Preprocessor Solution/solution.cpp
+++ c++/Other Concepts/Preprocessor Solution/solution.cpp
@@ -1,9 +1,9 @@
 // marcos here
 #define FUNCTION(name, operator) void name (int &current, int candidate) { ! (current operator candidate) ? current = candidate : false; }
 #define foreach(v, i) for(int i = 0; i < v.size(); i++)
-#define 
-
-
+#define io(v) cin >> v
+#define INF 10000000
+#define toStr(S) #S
 
 #include <iostream>
 #include <vector>
