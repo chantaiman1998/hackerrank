cc24e61cb96ef24b9f44c01d2eb108d6846deb6f
tai man chan <46643763+chantaiman1998@users.noreply.github.com>
2021-06-06 22:04:58 +0800

    Update solution.cpp

--- c++/Other Concepts/Medium/Attending Workshops/solution.cpp
+++ c++/Other Concepts/Medium/Attending Workshops/solution.cpp
@@ -33,7 +33,25 @@ int CalculateMaxWorkshops(Available_Workshops* data) {
     }
     return res;
 }
+// method end
 
-
-
-
+int main(int argc, char *argv[])
+{
+  int n;    // number of workshops
+  cin >> n;
+  // create arrays of unknown size n
+  int* start_time == new int[n];
+  int* duration == new int[n];
+  for(int i=0; i < n; i++)
+  {
+    cin >> start_time[i];
+  }
+  for(int i=0; i < n; i++)
+  {
+    cin >> duration[i];
+  }
+  Available_Workshops * ptr;
+  ptr = initialize(start_time, duration, n);
+  cout << CalculateMaxWorkshops(ptr)<< endl;
+  return 0;
+}
