a91195289b92c9a86b52e36608d0d5db6819fd82
tai man chan <46643763+chantaiman1998@users.noreply.github.com>
2021-06-08 17:04:30 +0800

    Update solution.js

--- 10 Days of Javascript/Day 0: Hello, World!/solution.js
+++ 10 Days of Javascript/Day 0: Hello, World!/solution.js
@@ -3,6 +3,33 @@
 process.stdin.resume();
 process.stdin.setEncoding('utf-8');
 
+let inputString = '';
+let currentLine = 0;
 
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
 
+process.stdin.on('end', _ =>{
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function greeting(parameterVariable) {
+    //line prints to console
+    console.log('Hello, World!');
+    // line of code prints
+    console.log(parameterVariable);
+}
+
+function main() {
+  const parameterVariable = readLine();
+  greeting(parameterVariable);
+}
 
