870f492b5a5600021117146b39e9f71c19afc062
tai man chan <46643763+chantaiman1998@users.noreply.github.com>
2021-06-11 16:34:23 +0800

    Update solution.cpp

--- Math/findthepoint/solution.cpp
+++ Math/findthepoint/solution.cpp
@@ -8,6 +8,24 @@ string rtrim(const string &);
 vector<string> split(const string &);
 
 /*
-  function is
+  function is expected to return an INTEGER_ARRAY.
+  The function accepts following parameters:
+  1. int px, py, qx, qy
+*/
+
+
+int main()
+{
+  ofstream fout(getenv("OUTPUT_PATH"));
+  
+  string n_temp;
+  getline(cin, n_temp);
+  
+  int n = stoi(ltrim(rtrim(n_temp)));
+  
+  for (int n_itr = 0; n_itr < n; n_itr++) {
+      string first_multiple_input_temp;
+      getline(cin, first_multiple_input_temp);
+    
 
 
