6330c05100ffcc8775e1b8281c5497457c839cdf
tai man chan <46643763+chantaiman1998@users.noreply.github.com>
2021-06-13 16:16:21 +0800

    Create solution.cpp

---
+++ Math/Fundamentals/Cutting Paper Squares/solution.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+int main() {
+  long int n, m;
+  cin >> n >> m;
+  cout << (n*m)-1 << endl;
+  return 0;
+}
+
