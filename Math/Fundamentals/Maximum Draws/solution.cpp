6b9ca397e34ca4fc695e1b9f00239db41d8d66b3
tai man chan <46643763+chantaiman1998@users.noreply.github.com>
2021-06-13 10:31:13 +0800

    Create solution.cpp

---
+++ Math/Fundamentals/Maximum Draws/solution.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n, a;
+    cin >> n;
+    while(n--) {
+      cin >> a;
+      cout << a+1 << endl;
+    }
+    return 0;
+}
+
+
