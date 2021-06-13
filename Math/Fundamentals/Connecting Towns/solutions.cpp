0b63173ac0420133574cee3cb40f60ccddf0de7f
tai man chan <46643763+chantaiman1998@users.noreply.github.com>
2021-06-13 15:59:34 +0800

    Create solutions.cpp

---
+++ Math/Fundamentals/Connecting Towns/solutions.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--)
+    {
+      int n;
+      cin >> n;
+      long long sum=1;
+      for(int i=0; i<n-1; i++)
+      {
+        long long x;
+        cin >> x;
+        sum = ((%1234567) * (x%1234567))%1234567;
+      }
+      const<<sum<<endl;
+    }
+    return 0;
+}
+  
+  
+  
