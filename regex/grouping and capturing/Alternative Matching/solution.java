6fcd7724774557ad808249b6dba788874cc26842
tai man chan <46643763+chantaiman1998@users.noreply.github.com>
2021-06-17 10:22:43 +0800

    Update solution.java

--- regex/grouping and capturing/Alternative Matching/solution.java
+++ regex/grouping and capturing/Alternative Matching/solution.java
@@ -6,4 +6,18 @@ import java.util.regex.*;
 
 public class Solution {
     public static void main(
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^(?:Mr|Mr?s|Dr|Er)\\.[a-zA-Z]+$");
+    }
+}
 
+
+class Regex_Test {
+    public void checker(String Regex_Pattern){
+        Scanner Input = new Scanner(System.in);
+        String Test_String = Input.nextLine();
+        Pattern p = Pattern.compile(Regex_Pattern);
+        Matcher m = p.matcher(Test_String);
+        System.out.println(m.find());
+    }   
+}
